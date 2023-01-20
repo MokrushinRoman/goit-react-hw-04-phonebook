84590cd5bf40a53b87a3cd15b91809da0e1ca895
Roman Mokrushin <emptyspaceman1@gmail.com>
2023-01-20 18:06:08 +0200

    adds start files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,106 +5,99 @@ import { Box } from '../Box';
 import { Title, Contacts, Phonebook, Filter } from 'components';
 
 export class App extends Component {
-    state = {
-        contacts: [
-            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ],
-        filter: '',
-    };
+  state = {
+    contacts: [
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
-    componentDidMount() {
-        const contacts = localStorage.getItem('contacts');
-        const parsedContacts = JSON.parse(contacts);
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
 
-        if (parsedContacts) {
-            this.setState({ contacts: parsedContacts });
-        }
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
     }
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.contacts !== prevState.contacts) {
-            localStorage.setItem(
-                'contacts',
-                JSON.stringify(this.state.contacts)
-            );
-        }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
+  }
 
-    addContact = (name, number) => {
-        const newContact = {
-            id: nanoid(),
-            name,
-            number,
-        };
-
-        this.setState(({ contacts }) => ({
-            contacts: [newContact, ...contacts],
-        }));
+  addContact = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
     };
 
-    dublicateFinder = newName => {
-        const { contacts } = this.state;
-        contacts.some(({ name }) => name === newName);
-    };
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
 
-    onFilterChange = e => {
-        this.setState({ filter: e.currentTarget.value });
-    };
+  dublicateFinder = newName => {
+    const { contacts } = this.state;
+    contacts.some(({ name }) => name === newName);
+  };
 
-    handleFilterContacts = () => {
-        const { contacts, filter } = this.state;
-        const normalizedFilter = filter.toLowerCase();
+  onFilterChange = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
 
-        return contacts.filter(({ name }) =>
-            name.toLowerCase().includes(normalizedFilter)
-        );
-    };
+  handleFilterContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
 
-    onDeleteContact = contactId => {
-        this.setState(({ contacts }) => ({
-            contacts: contacts.filter(({ id }) => id !== contactId),
-        }));
-    };
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
 
-    render() {
-        const { filter, contacts } = this.state;
-        const filteredContacts = this.handleFilterContacts();
+  onDeleteContact = contactId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
 
-        return (
-            <Box
-                mx="auto"
-                my="100px"
-                p={3}
-                width="500px"
-                border="1px solid rgba(0, 0, 0, 0.2)"
-                borderRadius="5px"
-                boxShadow="0 3px 7px rgba(0, 0, 0, 0.3)"
-            >
-                <h1 className="visually-hidden">Phonebook</h1>
+  render() {
+    const { filter, contacts } = this.state;
+    const filteredContacts = this.handleFilterContacts();
 
-                <Box>
-                    <Title text="phonebook" />
+    return (
+      <Box
+        mx="auto"
+        my="100px"
+        p={3}
+        width="500px"
+        border="1px solid rgba(0, 0, 0, 0.2)"
+        borderRadius="5px"
+        boxShadow="0 3px 7px rgba(0, 0, 0, 0.3)"
+      >
+        <h1 className="visually-hidden">Phonebook</h1>
 
-                    <Phonebook
-                        existedContacts={contacts}
-                        addContact={this.addContact}
-                    />
-                </Box>
+        <Box>
+          <Title text="phonebook" />
 
-                <Box mt={3}>
-                    <Title text="contacts" />
+          <Phonebook existedContacts={contacts} addContact={this.addContact} />
+        </Box>
 
-                    <Filter onChange={this.onFilterChange} value={filter} />
+        <Box mt={3}>
+          <Title text="contacts" />
 
-                    <Contacts
-                        contacts={filteredContacts}
-                        deleteContact={this.onDeleteContact}
-                    />
-                </Box>
-            </Box>
-        );
-    }
+          <Filter onChange={this.onFilterChange} value={filter} />
+
+          <Contacts
+            contacts={filteredContacts}
+            deleteContact={this.onDeleteContact}
+          />
+        </Box>
+      </Box>
+    );
+  }
 }
