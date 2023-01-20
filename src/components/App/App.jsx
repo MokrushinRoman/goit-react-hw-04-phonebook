fdc55e31e124066fbddb00eb9376767d402dc819
Roman Mokrushin <emptyspaceman1@gmail.com>
2023-01-20 18:49:28 +0200

    fixes bug

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ import { Title, Contacts, Phonebook, Filter } from 'components';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Hermione qwe', number: '443-89-12' },
+      { id: 'id-1', name: 'Hermione qwe', number: '443-89-2' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
