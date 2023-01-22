e6bb26daae09593567591e60e6248870a1a2853f
Roman Mokrushin <emptyspaceman1@gmail.com>
2023-01-22 15:45:36 +0200

    adds finished task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,104 +1,85 @@
-import { Component } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { nanoid } from 'nanoid';
 
 import { Box } from '../Box';
 import { Title, Contacts, Phonebook, Filter } from 'components';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Hermione qwe', number: '443-89-22' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const initialContacts = [
+    { id: 'id-1', name: 'Hermione qwe', number: '443-89-22' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+  const localStorageContacts = localStorage.getItem('contacts');
+  const parsedContacts = JSON.parse(localStorageContacts);
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  const [contacts, setContacts] = useState(
+    () => parsedContacts ?? initialContacts
+  );
+  const [filter, setFilter] = useState('');
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = (name, number) => {
+  const addContact = (name, number) => {
     const newContact = {
       id: nanoid(),
       name,
       number,
     };
 
-    this.setState(({ contacts }) => ({
-      contacts: [newContact, ...contacts],
-    }));
+    setContacts(contacts => [newContact, ...contacts]);
   };
 
-  dublicateFinder = newName => {
-    const { contacts } = this.state;
-    contacts.some(({ name }) => name === newName);
+  const onFilterChange = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  onFilterChange = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
+  const handleFilterContacts = useMemo(() => {
+    console.log('handleFilterContacts');
 
-  handleFilterContacts = () => {
-    const { contacts, filter } = this.state;
     const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(normalizedFilter)
     );
+  }, [contacts, filter]);
+
+  const onDeleteContact = contactId => {
+    setContacts(contacts => contacts.filter(({ id }) => id !== contactId));
   };
 
-  onDeleteContact = contactId => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(({ id }) => id !== contactId),
-    }));
-  };
 
-  render() {
-    const { filter, contacts } = this.state;
-    const filteredContacts = this.handleFilterContacts();
+  return (
+    <Box
+      mx="auto"
+      my="100px"
+      p={3}
+      width="500px"
+      border="1px solid rgba(0, 0, 0, 0.2)"
+      borderRadius="5px"
+      boxShadow="0 3px 7px rgba(0, 0, 0, 0.3)"
+    >
+      <h1 className="visually-hidden">Phonebook</h1>
 
-    return (
-      <Box
-        mx="auto"
-        my="100px"
-        p={3}
-        width="500px"
-        border="1px solid rgba(0, 0, 0, 0.2)"
-        borderRadius="5px"
-        boxShadow="0 3px 7px rgba(0, 0, 0, 0.3)"
-      >
-        <h1 className="visually-hidden">Phonebook</h1>
+      <Box>
+        <Title text="phonebook" />
 
-        <Box>
-          <Title text="phonebook" />
-
-          <Phonebook existedContacts={contacts} addContact={this.addContact} />
-        </Box>
-
-        <Box mt={3}>
-          <Title text="contacts" />
-
-          <Filter onChange={this.onFilterChange} value={filter} />
-
-          <Contacts
-            contacts={filteredContacts}
-            deleteContact={this.onDeleteContact}
-          />
-        </Box>
+        <Phonebook existedContacts={contacts} addContact={addContact} />
       </Box>
-    );
-  }
-}
+
+      <Box mt={3}>
+        <Title text="contacts" />
+
+        <Filter onChange={onFilterChange} value={filter} />
+
+        <Contacts
+          contacts={handleFilterContacts}
+          deleteContact={onDeleteContact}
+        />
+      </Box>
+    </Box>
+  );
+};
